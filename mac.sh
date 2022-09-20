fad94c8a796abee1a82809df6b41ae1d5e5bc662
Brandon Apol <brandonapol@cedarville.edu>
2022-09-20 16:58:45 -0700

    initial commit

---
+++ mac.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+chmod u+x mac.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install python3
+brew install node 
+brew install git 
+npm install -g truffle@5.4.2
+brew install ganache
+npm install -g node-gyp@3.6.2
+
+
+
+echo "@@@@@@@@@@@@@@@@&BP?~^#@@@@&~~?5B&@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@&5!.     !@@@@@@?     .!5&@@@@@@@@@@@@"
+echo "@@@@@@@@@&J.       7@@@@@@@@@@J       .J&@@@@@@@@@"
+echo "@@@@@@@#~         J@@@&&@@@@@@@5         ~#@@@@@@@"
+echo "@@@@@&~          .@@G7  7#@@@@@@.          ~&@@@@@"
+echo "@@@@5            !@?      .:^^P@J            P@@@@"
+echo "@@@?              BJ PBY  JBP J@              ?@@@"
+echo "@@J               ?@. .^77^. :@J               Y@@"
+echo "@B            .7P&Y&@?.7&&7:J@#5&P7.            #@"
+echo "@^          7&@@@@#.J@@@@@@@@J.#@@@@&!          ~@"
+echo "&          !@@@@@@@#. !B@@B! .&@@@@@@@!          @"
+echo "#          #@@@@@@@@@!      7@@@@@@@@@#          &"
+echo "&          @@@@@@@@@@@#^  ~#@@@@@@@@@@@          &"
+echo "@.         @@@@@@@@@@@@5?&@@@@@@@@@@@@@         .@"
+echo "@Y         #@@@#JYYYYYYJ55YYYYYYYY#@@@#         5@"
+echo "@@:        .B@@B                  #@@B.        ^@@"
+echo "@@&.   .J#&&#&@@?      P&&5      Y@@&##&B?    .@@@"
+echo "@@@@^ :@@@@@@@@@@^     !55~     !@@@@@@@@@&. ^@@@@"
+echo "@@@@@?5@@@@@@@@@@@^............~@@@@@@@@@@@JJ@@@@@"
+echo "@@@@@@@@@@@@@@@@@@YY~^~~^!GGGGPP@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@&?!!!!B@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@&     7@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@?~^:.Y@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "Happy Coding!"
---
+++ readme.md
@@ -0,0 +1,23 @@
+# CT Installs Packages - Blockchain
+
+Easy installation of requirements for CT's Blockchain coursework.
+
+# Running the Installs 
+## Mac/Linux:
+Open your terminal using cmd+spacebar and typing "Terminal". 
+
+In the terminal, copy and paste:
+```bash 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/brandonapol/ct-installs-scripts/master/mac.sh)"
+```
+
+## Windows 
+Open your command line interface. Copy and paste these TWO commands:
+```bash 
+$InstallFromCodingTemple = Invoke-WebRequest -Uri https://raw.githubusercontent.com/brandonapol/ct-installs-scripts/master/windows.ps1 -UseBasicParsing
+
+Invoke-Expression $($InstallFromCodingTemple.Content)
+```
+
+Should automatically install everything you need! Happy coding!
+    
